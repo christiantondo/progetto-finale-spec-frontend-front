649733d17446a3e28d297b813d6058d10797171d
Christian Tondo <christiantondo1@gmail.com>
2026-06-21 18:56:07 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+
+    return (
+        <>
+            <div>Welcome to LSV - Luxury & Sport Veichles</div>
+        </>
+    )
+}
+
+export default App
