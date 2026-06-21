ca92d07edcf35fc4262d68db9e5968c7b8e37f4e
Christian Tondo <christiantondo1@gmail.com>
2026-06-21 22:43:21 +0200

    detail page fixed and data received

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,11 +1,22 @@
 import { useParams } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { GlobalContext } from "../context/GlobalContext";
 
 export function DetailPage() {
     const { id } = useParams();
-    const { vehicles } = useContext(GlobalContext);
-    const vehicle = vehicles.find(v => v.id === parseInt(id));
+    const [vehicle, setVehicle] = useState(null);
+
+    useEffect(() => {
+        const { VITE_API_URL } = import.meta.env;
+        fetch(`${VITE_API_URL}/vehicles/${id}`)
+            .then(res => res.json())
+            .then(data => setVehicle(data.vehicle))
+            .catch(err => console.error("Cannot get data", err));
+    }, [id]);
+
+    if (vehicle === null) {
+        return <h2>Loading...</h2>
+    }
 
     if (!vehicle) {
         return (
@@ -14,6 +25,15 @@ export function DetailPage() {
     }
 
     return (
-        <div>Detail Page</div>
+        <div>
+            <h2>{vehicle.title}</h2>
+            <p>Price: {vehicle.price}</p>
+            <p>Brand: {vehicle.brand}</p>
+            <p>Release Year: {vehicle.releaseYear}</p>
+            <p>Total Km: {vehicle.km}</p>
+            <p>Previous owners: {vehicle.owners}</p>
+            <p>Fuel type: {vehicle.fuelType}</p>
+            <p>Engine size: {vehicle.engineSize}</p>
+        </div>
     )
 }
