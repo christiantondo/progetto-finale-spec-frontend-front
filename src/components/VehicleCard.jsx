170971c2eb8f9fe3ff214d4028808f84835fab9e
Christian Tondo <christiantondo1@gmail.com>
2026-06-23 00:19:48 +0200

    minor

--- src/components/VehicleCard.jsx
+++ src/components/VehicleCard.jsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 export default function VehicleCard({ vehicle }) {
     return (
         <div className="card">
-            <h3>{vehicle.title}</h3>
-            <span>{vehicle.category}</span>
+            <div>
+                <h3>{vehicle.title}</h3>
+                <span>{vehicle.category}</span>
+            </div>
 
             <Link to={`/vehicles/${vehicle.id}`} className="detail-button">Discover more</Link>
 
