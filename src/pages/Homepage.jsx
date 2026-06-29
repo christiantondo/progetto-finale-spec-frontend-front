58034659d0e25da21a9a19ce97b73d48e0b9153a
Christian Tondo <christiantondo1@gmail.com>
2026-06-29 21:09:42 +0200

    new look for homepage, now the logo is displayed in the middle of the page

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,14 @@
 export default function Homepage() {
     return (
-        <div>
+        <div className="home-container">
+            <div className="home-logo-wrapper">
+                <img
+                    src="/LSV-nobg.png"
+                    alt="LSV Luxury & Sport Vehicles"
+                    className="home-central-logo"
+                    draggable="false"
+                />
+            </div>
         </div>
-    )
+    );
 }
