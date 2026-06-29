2136ea449cb7c3c3736d18abf6d21aefcb89c261
Christian Tondo <christiantondo1@gmail.com>
2026-06-29 21:20:21 +0200

    added poster for preventing black background at page refresh

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,9 +1,18 @@
-import videoBg from "../assets/videoBG.mov"
+import videoBg from "../assets/videoBG.mov";
+import firstFrame from "../assets/firstframe.png";
 
 export default function VideoBackground() {
     return (
         <div className="videoBg">
-            <video src={videoBg} autoPlay loop muted playsInline></video>
+            <video
+                src={videoBg}
+                autoPlay
+                loop
+                muted
+                playsInline
+                poster={firstFrame}
+                preload="auto"
+            ></video>
         </div>
     )
 }
